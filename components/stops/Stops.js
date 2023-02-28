01f2aff120c535cadb63d579c2b7857ecb96d6a8
Will <wilfredopaizpaz@gmail.com>
2023-02-28 17:09:04 +0100

    avance

---
+++ components/stops/Stops.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+
+const Stops = () => {
+  return (
+    <View>
+        <Text>Stops...</Text>
+    </View>
+  )
+}
+
+export default Stops
